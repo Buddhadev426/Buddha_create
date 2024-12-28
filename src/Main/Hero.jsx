1a68f77e0a6c0fbfa2c7ef392968804e89d17503
buddhadev maity <buddhadev09101996@gmail.com>
2024-12-28 21:52:59 +0530

    change the hero title

--- src/Main/Hero.jsx
+++ src/Main/Hero.jsx
@@ -62,7 +62,7 @@ function Hero() {
           Spotlight <br /> is the new trend.
         </h1>
         <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-          Spotlight effect is a great way to draw attention to a specific part
+          Hi, this is Buddhadev. The Spotlight effect is a great way to draw attention to a specific part
           of the page. Here, we are drawing the attention towards the text
           section of the page. I don&apos;t know why but I&apos;m running out of
           copy.
